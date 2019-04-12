fbe6f1657fdb15b748cd0dc12956950f66d9d207
romanPashnitskyi <bernald_07@meta.ua>
2019-04-12 05:00:40 +0300

    add promise chain

--- src/main.js
+++ src/main.js
@@ -1,32 +1,20 @@
-const $list = document.getElementById('list');
-
-const list = point => {
-  const $element = document.createElement('element');
-  $element.className = 'item';
-  $element.innerHTML = `
-    <p>ID : ${point.id}</p>
-    <p>Title : ${point.title}</p>
-    <p>Body : ${point.body}</p>     
-    <hr/>
-  `;
-  $list.appendChild($element);
-};
-
-const arr = () => {
-  const arrayURL = [];
-  for (let i = 1; i <= 10; i++) {
-    arrayURL.push('http://jsonplaceholder.typicode.com/posts/' + i);
-  }
-
-  return arrayURL;
-};
-
-async function chain (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    const response = await fetch(arr[i]);
-    const data = await response.json();
-    list(data);
-  }
+function chain () {
+  fetch('http://jsonplaceholder.typicode.com/posts/')
+    .then(response => response.json())
+    .then(data => {
+      for (let i = 0; i <= 9; i++) {
+        const $element = document.createElement('element');
+        $element.className = 'item';
+        $element.innerHTML = `
+            <p>ID : ${data[i].id}</p>
+            <p>Title : ${data[i].title}</p>
+            <p>Body : ${data[i].body}</p>     
+            <hr/>
+          `;
+        const $list = document.getElementById('list');
+        $list.appendChild($element);
+      }
+    });
 }
 
-chain(arr());
+chain();
